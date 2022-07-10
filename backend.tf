9440875bb0ce4d45f4b32213dfd9fd6c30adfa63
Arkajyoti Misra <arkajyoti.misra@gmail.com>
2022-07-10 14:54:05 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket"
+    bucket = "mydev-tf-state-bucket-654321"
     key = "main"
-    region = "us-east-2"
-    dynamodb_table = "my-dynamodb-table"
+    region = "us-west-2"
+    dynamodb_table = "my-dynamodb-table-654321"
   }
 }
