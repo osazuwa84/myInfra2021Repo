e2b84c52dc948ae9be1a29df1f1531c5b82934ca
osazuwa84 <56798927+osazuwa84@users.noreply.github.com>
2022-07-30 18:51:07 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "jen_tfs" 
+    default     =  "NVirginiaKey" 
 }
 
 variable "instance_type" { 
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" { 
     description = "Name of security group" 
-    default     = "jenkins-sg-abc" 
+    default     = "all protocol" 
 }
 
 variable "tag_name" { 
