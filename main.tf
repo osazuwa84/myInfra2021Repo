d896a51103381780387e9a4f17004f94fb5c75cb
osazuwa84 <56798927+osazuwa84@users.noreply.github.com>
2022-07-30 18:43:34 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 #Create security group with firewall rules
-resource "aws_security_group" "my_security_group" {
+resource "aws_security_group" "all protocol" {
   name        = var.security_group
   description = "security group for Ec2 instance"
 
